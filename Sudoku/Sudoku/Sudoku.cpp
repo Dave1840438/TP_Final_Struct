7b8872648d75efc685d9b154479f1ad51a2dc4ba
Dave1840438 <david.sylvestre@sympatico.ca>
2014-12-09 09:42:31 -0500

    lol charlie

--- Sudoku/Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku/Sudoku.cpp
@@ -147,6 +147,7 @@ bool Sudoku::Solutionner()
 
 void Sudoku::EnleverPossibilités()
 {
+
 	for (int i = 0; i < DIMENSION_MATRICE; i++)
 	{
 		for (int j = 0; j < DIMENSION_MATRICE; j++)
@@ -159,7 +160,7 @@ void Sudoku::EnleverPossibilités()
 						|| EstDansQuadrant(*it, i, j))
 					{
 						matricePossibilités_[i][j].erase(it);
-						it--;
+                  it--;
 					}
 				}
 			}
@@ -207,8 +208,6 @@ bool Sudoku::EstDansQuadrant(int nombre, int x, int y)
 
 bool Sudoku::MatriceEstPleine()
 {
-	bool resultat = true;
-
 	for (int i = 0; i < DIMENSION_MATRICE; i++)
 	{
 		for (int j = 0; j < DIMENSION_MATRICE; j++)
